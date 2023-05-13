6560541bd325df75a60eb01994d2035677973199
Naquoc17 <naquoc17.konto@gmail.com>
2023-05-13 17:08:46 +0200

    fix app.js

--- app.js
+++ app.js
@@ -164,7 +164,14 @@ async function main() {
 //   res.render("about");
 // });
 
-app.listen(3000, function () {
+
+
+let port = process.env.PORT;
+if (port == null || port = ""){
+    port = 3000;
+}
+
+app.listen(port, function () {
     console.log("Server is ready.");
 })
 
